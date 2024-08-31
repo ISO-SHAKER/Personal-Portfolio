600c91e18a0b56013defb2ef86feed00b44cc0d6
Islam Shaker <153021889+ISO-SHAKER@users.noreply.github.com>
2024-08-31 16:01:21 +0300

    Update projects.route.js

--- Backend/routes/projects.route.js
+++ Backend/routes/projects.route.js
@@ -9,12 +9,12 @@ const router = express.Router();
 router
   .route("/")
   .get(projectsController.getProjects)
-  .post(upload.single("imageSrc"), projectsController.addProject);
+  .post(upload.single("imgSrc"), projectsController.addProject);
 
 router
   .route("/:id")
   .get(projectsController.getProject)
-  .patch(upload.single("imageSrc"), projectsController.updateProject)
+  .patch(upload.single("imgSrc"), projectsController.updateProject)
   .delete(projectsController.deleteProject);
 
 module.exports = router;
